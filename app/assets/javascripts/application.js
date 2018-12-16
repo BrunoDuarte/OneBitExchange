17317430924b30f68ec388d3b219102fa0f112a8
BrunoDuarte <frank.bruno555@gmail.com>
2018-12-16 11:34:11 -0300

    commit test to heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,7 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-
+//
 //= require jquery
 //= require tether/dist/js/tether
 //= require bootstrap/dist/js/bootstrap.bundle
