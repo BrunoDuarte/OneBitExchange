4a8bec0f86f9ef822b658ca066d34ef7fdddcd8b
BrunoDuarte <frank.bruno555@gmail.com>
2018-12-26 16:00:53 -0300

    Added github exchange

--- app/services/exchange_service.rb
+++ app/services/exchange_service.rb
@@ -11,13 +11,32 @@ class ExchangeService
  
   def perform
     begin
-      exchange_api_url = Rails.application.credentials[Rails.env.to_sym][:currency_api_url]
-      exchange_api_key = Rails.application.credentials[Rails.env.to_sym][:currency_api_key]
-      url = "#{exchange_api_url}?token=#{exchange_api_key}&currency=#{@source_currency}/#{@target_currency}"
-      res = RestClient.get url
-      value = JSON.parse(res.body)['currency'][0]['value'].to_f
-      
-      value * @amount
+      if @source_currency == 'BTC' || @target_currency == 'BTC'
+        if @source_currency == 'BTC'
+          bitcoin_api_url = Rails.application.credentials[Rails.env.to_sym][:bitcoin_api_url]
+          url = "#{bitcoin_api_url}#{@target_currency}.json"
+          res = RestClient.get url
+          value = JSON.parse(res.body)['bpi'][@target_currency]['rate_float']
+
+          @amount * value
+        else
+          bitcoin_api_url = Rails.application.credentials[Rails.env.to_sym][:bitcoin_api_url]
+          url = "#{bitcoin_api_url}#{@source_currency}.json"
+          res = RestClient.get url
+          value = JSON.parse(res.body)['bpi'][@source_currency]['rate_float']
+
+          @amount / value
+        end
+
+      else
+        exchange_api_url = Rails.application.credentials[Rails.env.to_sym][:currency_api_url]
+        exchange_api_key = Rails.application.credentials[Rails.env.to_sym][:currency_api_key]
+        url = "#{exchange_api_url}?token=#{exchange_api_key}&currency=#{@source_currency}/#{@target_currency}"
+        res = RestClient.get url
+        value = JSON.parse(res.body)['currency'][0]['value'].to_f
+        
+        value * @amount
+      end
     rescue RestClient::ExceptionWithResponse => e
       e.response
     end
